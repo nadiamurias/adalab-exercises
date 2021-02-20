f15397a685b5a3b70ef5056b901ec7fd8a139802
Nadia Murias <nadiamurias@gmail.com>
2021-02-20 23:20:28 +0100

    Create component and use props

---
+++ modulo-3/leccion-1-ejercicio-1-2-3/src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+
+class App extends React.Component {
+  render() {
+    const header = (
+      <header className="header">
+        <img src={logo} className="logo" alt="logo" />
+        <div className="header-text">
+          <h1 className="name">Nadia Murias</h1>
+          <h2 className="date">Martes, 16 de Febrero de 2021</h2>
+        </div>
+      </header>
+    );
+    const main = (
+      <main className="main">
+        <p className="main-text">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum
+          repellendus sapiente sint ut officia molestiae incidunt facere magnam
+          minus, aliquid eos animi, soluta sequi harum? Odio non in ducimus
+          aspernatur. Rerum repellendus sapiente sint ut officia molestiae
+          incidunt facere magnam minus, aliquid eos animi, soluta sequi harum?
+          Odio non in ducimus aspernatur...
+        </p>
+      </main>
+    );
+    const footer = (
+      <footer className="footer">
+        <a className="read-more">Leer más...</a>
+        <p className="likes">
+          37<i className="fas fa-heart"></i>
+        </p>
+      </footer>
+    );
+    const card = (
+      <div className="card">
+        {header} {main} {footer}
+      </div>
+    );
+    const appRoot = <div className="App">{card}</div>;
+    return appRoot;
+  }
+}
+
+export default App;
--- modulo-3/leccion-2-ejercicio-1/src/components/MediaCard.js
+++ modulo-3/leccion-2-ejercicio-1/src/components/MediaCard.js
@@ -1,43 +1,39 @@
 import React from "react";
-import "../stylesheets/App.scss";
+import logo from "../logo.svg";
+import "../App.scss";
 
-const headerTitle = <h1 className="App--header-name-title">Nadia Murias</h1>;
-const headerText = (
-  <p className="App--header-name-text">Martes 16 de febrero, 2021</p>
-);
-const mainText = (
-  <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  </p>
-);
-function MediaCard() {
-  const section = (
-    <section className="App--header-name">
-      {headerTitle}
-      {headerText}
-    </section>
-  );
-  return (
-    <div className="App">
-      <section className="App--card">
-        <header className="App--header">
-          {/* <img src={logo} className="App--header-img" alt="logo" /> */}
-          {section}
-        </header>
-        <main className="App--main">{mainText}</main>
-        <footer className="App--footer">
-          <p>Leer más...</p>
-          <p>
-            37 <i className="fas fa-heart App--footer-icon"></i>
-          </p>
-        </footer>
-      </section>
-    </div>
-  );
+class MediaCard extends React.Component {
+  render() {
+    const header = (
+      <header className="header">
+        <img src={this.props.image} className="logo" alt="logo" />
+        <div className="header-text">
+          <h1 className="name">{this.props.username}</h1>
+          <h2 className="date">{this.props.date}</h2>
+        </div>
+      </header>
+    );
+    const main = (
+      <main className="main">
+        <p className="main-text">{this.props.text}</p>
+      </main>
+    );
+    const footer = (
+      <footer className="footer">
+        <a className="read-more">Leer más...</a>
+        <p className="likes">
+          {this.props.likes} <i className="fas fa-heart"></i>
+        </p>
+      </footer>
+    );
+    const card = (
+      <div className="card">
+        {header} {main} {footer}
+      </div>
+    );
+    const appRoot = <div className="App">{card}</div>;
+    return appRoot;
+  }
 }
+
 export default MediaCard;
